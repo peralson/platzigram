979de2da3b93675689887bb9afb2fd6b6c0b27ab
Pablo Trinidad <pabloatp0204@gmail.com>
2018-07-09 11:44:52 -0500

    7 - El objeto request.

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,16 +1,13 @@
 """Platzigram URLs module."""
 
 from django.urls import path
-from django.http import HttpResponse
 
-
-def hello_world(request):
-    """Return a greeting."""
-    return HttpResponse('Hello, world!')
+from platzigram import views
 
 
 urlpatterns = [
 
-    path('hello-world/', hello_world)
+    path('hello-world/', views.hello_world),
+    path('hi/', views.hi),
 
 ]
