b4f1317df21facd8901e46d896fd8b76ee40aa5c
Pablo Trinidad <pabloatp0204@gmail.com>
2018-07-11 10:33:04 -0500

    23 - Logout

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 """Users views."""
 
 # Django
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 
@@ -18,3 +19,10 @@ def login_view(request):
             return render(request, 'users/login.html', {'error': 'Invalid username and password'})
 
     return render(request, 'users/login.html')
+
+
+@login_required
+def logout_view(request):
+    """Logout a user."""
+    logout(request)
+    return redirect('login')
